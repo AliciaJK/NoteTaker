8dce75ecedf62f0247950f540d695a757fc87e22
AliciaJK <alicia.krolak4@gmail.com>
2021-05-18 19:36:41 -0400

    attempt 4 bug fix html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,9 @@
 // We need to include the path package to get the correct file path for our html
 
 const path = require('path');
-
+const router = require('express').Router();
+const fs = require("fs");
+const noteList = JSON.parse(fs.readFileSync("db/db.json"));
 // ROUTING
 
 module.exports = (app) => {
@@ -10,23 +12,20 @@ module.exports = (app) => {
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
 
-  app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../notes.html'));
-  });
-
-  // app.get('/', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '../index.html'));
-  // });
-
 
   // Retrieves a note with specific id
-  app.get("/api/notes/:id", function (req, res) {
+  router.get("/notes/:id", function (req, res) {
+    // display json for the notes array indices of the provided id
+    res.json(notes[req.params.id]);
+  });
+
+  router.get("/notes/", function (req, res) {
     // display json for the notes array indices of the provided id
     res.json(notes[req.params.id]);
   });
 
   // If no matching route is found default to home
-  app.get('*', (req, res) => {
+  router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
   });
 };
