36ccb3917fba75f30e219adb36000b0f10e1b616
AliciaJK <alicia.krolak4@gmail.com>
2021-05-18 21:19:19 -0400

    attempt 11 - I think I am running out of steam

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,9 +9,25 @@ const fs = require("fs");
 const noteList = JSON.parse(fs.readFileSync("db/db.json"));
 
 
+// pushing to the DB-----------------------
+function writeToDB(notes){
+  // Converts new JSON Array back to string
+  notes = JSON.stringify(notes);
+  console.log (notes);
+  // Writes String back to db.json
+  fs.writeFileSync("./db/db.json", notes, function(err){
+      if (err) {
+          return console.log(err);
+      }
+  // });
+
+});
+}
+  writeToDB(notesData);
 
 
-app.get('/notes', (req, res) => {
+
+router.get('/notes', (req, res) => {
   const noteList = JSON.parse(fs.readFileSync("db/db.json"));
   return res.json(noteList);
 
@@ -23,7 +39,7 @@ app.get('/notes', (req, res) => {
 // ROUTING
 
 // module.exports = (app) => {
-app.post('/api/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
   // res.json(tableData));
 
   const newNote = {
@@ -42,40 +58,35 @@ app.post('/api/notes', (req, res) => {
 
 
   //delete note-------------------------
-  router.delete('/api/notes/:id', (req, res) => {
-  //finds note by id, then converts the string into a JSON object with the id parameters of the request made
-  let findNote = noteList.find(({ id }) => id === JSON.parse(req.params.id));
-  // const noteId = parseInt(req.params.id)
+//   router.delete('/api/notes/:id', (req, res) => {
+//   //finds note by id, then converts the string into a JSON object with the id parameters of the request made
+//   let findNote = noteList.find(({ id }) => id === JSON.parse(req.params.id));
+//   // const noteId = parseInt(req.params.id)
+//   const noteList = JSON.parse(fs.readFileSync("db/db.json"));
+//   const newArray = noteList.filter(note => note.id !== noteId);
+  
+  
+//   noteList.splice(noteList.indexOf(findNote), 1);
+//   fs.writeFileSync("db/db.json", JSON.stringify(newArray));
+
+//    res.json({ok: true});
+//   //Delete object matching the index of the note ID
+
+//   res.end("Note was deleted");
+// });
+
+// module.exports = router;
+router.delete('/notes/:id', (req, res) => {
+  console.log(req.params)
+  const noteId = parseInt(req.params.id)
   const noteList = JSON.parse(fs.readFileSync("db/db.json"));
   const newArray = noteList.filter(note => note.id !== noteId);
-  
-  
-  noteList.splice(noteList.indexOf(findNote), 1);
+
   fs.writeFileSync("db/db.json", JSON.stringify(newArray));
 
    res.json({ok: true});
-  //Delete object matching the index of the note ID
-
-  res.end("Note was deleted");
 });
-
+//exports the  crud actions to the server 
 module.exports = router;
 
-
-
-// pushing to the DB-----------------------
-function writeToDB(notes){
-  // Converts new JSON Array back to string
-  notes = JSON.stringify(notes);
-  console.log (notes);
-  // Writes String back to db.json
-  fs.writeFileSync("./db/db.json", notes, function(err){
-      if (err) {
-          return console.log(err);
-      }
-  // });
-
-});
-}
-  writeToDB(notesData);
 
